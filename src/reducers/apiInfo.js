29e25b267b23c25aa1cbebad0e6b381b344c637d
ccxex29 <blackccxex@gmail.com>
2020-12-31 16:29:24 +0700

    Apply changes to fit into production build

--- src/reducers/apiInfo.js
+++ src/reducers/apiInfo.js
@@ -1,4 +1,12 @@
-const apiReducer = (state = 'http://localhost:3099', action) => {
+const apiUri = () => {
+    if (process.env.API === 'development') {
+        console.warn('Development Mode');
+        return 'http://localhost:3099';
+    }
+    return 'https://reet-api.herokuapp.com';
+}
+
+const apiReducer = (state = apiUri(), action) => {
     return state;
 }
 
